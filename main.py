db14eb5d73f45f50725a532bdfe2903a46abfc11
Brian <brianzou03@gmail.com>
2021-08-30 22:56:08 -0400

    2nd Commit - Sentence class created, word file added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# Python natural language toolkit (NLTK)
+# Uses Python natural language toolkit (NLTK)
+# English words txt file from: https://github.com/dwyl/english-words
 
 from PyDictionary import PyDictionary
 
@@ -7,8 +8,7 @@ from PyDictionary import PyDictionary
 # nltk.download('wordnet')
 from nltk.corpus import wordnet as wn
 
-
-word_list = ['amazing', 'interesting', 'love', 'great', 'nice']
+word_list = ['amazing', 'interesting', 'love', 'great', 'nice']  # replace w/ txt file -> needs conversion
 pos_all = dict()
 
 for word in word_list:
@@ -31,4 +31,37 @@ print(contains_s)
 key_1 = list(pos_all.keys()).index('interesting')
 
 print(key_1)
+
+''' Sentence Patterns:
+Subject - Verb - Object
+Subject - Verb
+Subject - Verb - Adjective
+Subject - Verb - Adverb '''
+
+
+class Sentence:
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+
+    def open_file(self):
+        open_words = open("words.txt", "r")
+
+        print(open_words)
+
+    def joiner(self):
+        print(self.value1.capitalize() + " " + self.value2 + ".")
+
+
+my_obj = Sentence(word_list[1], word_list[2])
+my_obj.joiner()
+
+# TODO: CREATE SENTENCES VIA LOOP, FILTERS OUT INVALID SENTENCES, STOPS AT 1000 WORDS
+# EXPLAIN IN README EXPANSION POTENTIAL
+# PROOF OF CONCEPT
+
+open_words = open("words.txt", "r")
+
+print(open_words)
+
 
